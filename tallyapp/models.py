ae65b6225d3bb4745dadecadefb32bdebf5c9519
sajith3210 <sajithjanardhanan123@gmail.com>
2022-07-16 17:12:34 +0530

    tly

--- tallyapp/models.py
+++ tallyapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class stock_group(models.Model):
+    stock_group_name=models.CharField(max_length=100)
+    alias=models.CharField(max_length=10)
+    under=models.CharField(max_length=100)
+    should_quantity_of_items_be_added=models.CharField(max_length=5)
+
--- tallyapp/views.py
+++ tallyapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-
+from  tallyapp.models import *
 # Create your views here.
 def index(request):
 
     return render(request,'index.html')
 
 def stockgroup(request):
-    return render(request,'stock group creation.html')
+    stock_grou=stock_group.objects.all()
+    if request.method=="POST":
+        nme=request.POST['stock_group_name']
+        alia=request.POST['stock_group_alias']
+        und=request.POST['stock_group_under']
+        should_quantity_item=request.POST['quantity_item']
+
+        if stock_grou.objects.get(stock_group_name=nme).exists():
+            
+        su=stock_group(stock_group_name=nme,alias=alia,under=und,should_quantity_of_items_be_added=should_quantity_item) 
+        su.save()  
+    return render(request,'stock group creation.html',{'st':stock_grou})
 
 def stockcategory(request):
     return render(request,'stock category creation.html')
